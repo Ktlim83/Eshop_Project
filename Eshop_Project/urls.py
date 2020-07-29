4d0399274d4f5f583342d4ed30d00acc3b912f6a
KEITH LIM <63866293+Kweef206@users.noreply.github.com>
2020-07-29 13:02:11 -0700

    added html files and css

--- Eshop_Project/urls.py
+++ Eshop_Project/urls.py
@@ -14,8 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.shortcuts import redirect
+
+def index(request):
+    return redirect('/products')
 
 urlpatterns = [
+    
+    path('',index),
+    path('products/', include('apps.products_main.urls')),
     path('admin/', admin.site.urls),
 ]
---
+++ apps/products_main/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ProductsMainConfig(AppConfig):
+    name = 'products_main'
